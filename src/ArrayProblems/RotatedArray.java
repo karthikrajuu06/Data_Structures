ca81c71d1e891ce62a1e2e9da6cb1cd2fd8670a3
kartheek atmakuru <kartheekatmakuru@gmail.com>
2026-08-29 10:14:17 +0530

    update

---
+++ src/ArrayProblems/RotatedArray.java
@@ -0,0 +1,29 @@
+package ArrayProblems;
+
+import java.util.Scanner;
+
+public class RotatedArray {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		 int arr[]= new int [n];
+		 for(int i=0;i<n;i++) {
+			 arr[i] = sc.nextInt();
+		 }
+		 
+		 for(int j=0;j<3;j++) {    // Here J is used how many times to rotate an array
+			 int temp = arr[n-1];    // j is tells number of rotations 
+			 for(int i=n-1;i>0;i--) {
+				 arr[i] = arr[i-1];
+			 }
+				 arr[0]=temp;
+		 }
+		 
+		 for(int i=0; i<n; i++) {
+			 System.out.println(arr[i]);
+		 }
+
+	}
+
+}
